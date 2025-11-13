f4efc9de094ce92fb04a489914fee8e72e861dd9
Batya-Boss <t9161701227@yandex.ru>
2025-11-13 23:44:15 +0300

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,17 @@
+// В этом файле объявления функций.
+// calculator.h
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+#include <iostream>
+#include <string>
+
+using Number = double;
+
+// Функция чтения числа
+bool ReadNumber(Number& result);
+
+// Функция запуска цикла калькулятора
+bool RunCalculatorCycle();
+
+#endif // CALCULATOR_H_
