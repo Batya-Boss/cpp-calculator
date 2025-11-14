33b4aef817413837f7f9ca79d09bada44499d82b
Batya-Boss <t9161701227@yandex.ru>
2025-11-14 10:21:24 +0300

    Delete cpp-calculator directory

--- cpp-calculator/cpp-calculator/main.cpp
+++
@@ -1,7 +0,0 @@
-// В этом файле только функция main и #include-директивы.
-#include "calculator.h"
-
-int main() {
-    bool success = RunCalculatorCycle();
-    return !success; // Вернуть 0 при успешном завершении, иначе 1
-}
